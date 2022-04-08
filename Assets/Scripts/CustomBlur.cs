f7d72cd0c5b42617f3ea132fb6a8da327ea4402d
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2022-04-08 15:28:39 +0200

    changed somethings of the code

--- Assets/Scripts/CustomBlur.cs
+++ Assets/Scripts/CustomBlur.cs
@@ -11,21 +11,24 @@ using UnityEngine.Rendering.PostProcessing;
             PostProcessEvent.AfterStack,//Tells Unity when to execute this postpro in the stack
             "Custom/Blur")] //Creates a menu entry for the effect
                             //Forth parameter that allows to decide if the effect should be shown in scene view
-public sealed class BlurSettings : PostProcessEffectSettings
+public sealed class CustomBlurSettings : PostProcessEffectSettings
 {
-    [Range(0f, 1f), Tooltip("Intensity value")]
-    public FloatParameter blend = new FloatParameter { value = 0.0f }; //Custom parameter class, full list at: /PostProcessing/Runtime/
-                                                                       //The default value is important, since is the one that will be used for blending if only 1 of volume has this effect
+    [Range(0f, 1f), Tooltip("Blur Intensity.")]
+    public FloatParameter blur = new FloatParameter { value = 0.1f }; //Custom parameter class, full list at: /PostProcessing/Runtime/
+                                                                      //The default value is important, since is the one that will be used for blending if only 1 of volume has this effect
+    //[Range(10f, 30f), Tooltip("Blur Cuantity.")]
+    //public FloatParameter cuantity = new FloatParameter { value = 0.1f };
 }
 
-public class CustomBlur : PostProcessEffectRenderer<BlurSettings>//<T> is the setting type
+public class CustomBlur : PostProcessEffectRenderer<CustomBlurSettings>//<T> is the setting type
 {
     public override void Render(PostProcessRenderContext context)
     {
         //We get the actual shader property sheet
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Blur"));
         //Set the uniform value for our shader
-        sheet.properties.SetFloat("_intensity", settings.blend);
+        sheet.properties.SetFloat("_intensity", settings.blur);
+        //sheet.properties.SetFloat("_cuantity", settings.cuantity);
 
         //We render the scene as a full screen triangle applying the specified shader
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
--- Assets/Shaders/CustomBlur.shader
+++ Assets/Shaders/CustomBlur.shader
@@ -3,10 +3,15 @@ Shader "Hidden/Custom/Blur"
 	HLSLINCLUDE
 		// StdLib.hlsl holds pre-configured vertex shaders (VertDefault), varying structs (VaryingsDefault), and most of the data you need to write common effects.
 #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
-
+#define PI 3.14159265359
+#define E 2.71828182846
 		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
 
 	float _intensity;
+	float _cuantity = 30;
+
+	float _StandardDeviation = 0.02;
+
 	float4 Frag(VaryingsDefault i) : SV_Target
 	{
 		//float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
@@ -20,16 +25,44 @@ Shader "Hidden/Custom/Blur"
 		//init color variable
 		float4 col = 0;
 		//iterate over blur samples
-		for (float index = 0; index < 10; index++) {
+		for (float index = 0; index < 30; index++) 
+		{
 			//get uv coordinate of sample
-			float2 uv = i.texcoord + float2((index / 9 - 0.5) * 0.1 * invAspect, 0);
+			float2 uv = i.texcoord + float2((index / (30 - 1) - 0.5) * _intensity * invAspect, 0);
 			//add color at position to color
 			col += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv);
 		}
+
+		for (float index2 = 0; index2 < 30; index2++) 
+		{
+			//get uv coordinate of sample
+			float2 uv = i.texcoord + float2(0, (index2 / (30 - 1) - 0.5) * _intensity * invAspect);
+			//add color at position to color
+			col += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv);
+		}
+
 		//divide the sum of values by the amount of samples
-		col = col / 10;
+		col = col / (30 * 2);
 		return col;
 
+		//Parte de Gauss (aun se tiene que probar)
+		float sum = 0;
+
+		for (float index3 = 0; index3 < 30; index3++) 
+		{
+			float offset = (index3 / (30 - 1) - 0.5) * _intensity;
+
+			float2 uv = i.texcoord + float2(0, offset);
+
+			float stDevSquared = _StandardDeviation * _StandardDeviation;
+			float gauss = (1 / sqrt(2 * PI * stDevSquared)) * pow(E, -((offset * offset) / (2 * stDevSquared)));
+
+			sum += gauss;
+			col += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv) * gauss;
+		}
+
+		col = col / sum;
+		return col;
 
 	}
 		ENDHLSL
